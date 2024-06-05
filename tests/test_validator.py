2617c489dcabd3db1988fc5b4c2dc67e3939f907
Viktor Bubanja <viktor.bubanja@heyjobs.de>
2024-06-05 12:56:17 +0200

    Add tests for validator and root_validator decorators

---
+++ tests/test_validator.py
@@ -0,0 +1,48 @@
+from src.main import BaseModel
+from src.validator import validator, root_validator
+
+
+def test_validator_handles_multiple_fields():
+    class TestModel(BaseModel):
+        a: str
+        b: str
+
+        @validator("a", "b")
+        def lower_case(cls, v):
+            return v.lower()
+
+    test_model = TestModel(a="AAA", b="BBB")
+    assert test_model.a == "aaa"
+    assert test_model.b == "bbb"
+
+
+def test_validator_handles_applying_multiple_validators_to_single_field():
+    class TestModel(BaseModel):
+        a: str
+
+        @validator("a")
+        def surround_with_x(cls, v):
+            return f"x{v}x"
+
+        @validator("a")
+        def surround_with_y(cls, v):
+            return f"y{v}y"
+
+    test_model = TestModel(a="aaa")
+    assert test_model.a == "yxaaaxy"
+
+
+def test_root_validator_modifies_input_kwargs():
+    class TestModel(BaseModel):
+        first_name: str
+        last_name: str
+        full_name: str
+
+
+        @root_validator()
+        def build_full_name(cls, values):
+            values["full_name"] = values["first_name"] + " " + values["last_name"]
+            return values
+
+    test_model = TestModel(first_name="Uncle", last_name="Bob")
+    assert test_model.full_name == "Uncle Bob"
